fb95f5d73a2ad7dd8b394753976079fc4399f1f1
Bryan Liu <bryanliu@macbook-pro.attlocal.net>
2018-11-17 16:19:00 -0800

    onclick function started

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,11 @@
 document.getElementById("StepInto").onclick = function() {myFunction()};
 
 function myFunction() {
-    console.log("hello")
+  
+     	
+$( "#triviaQuiz" ).empty();
+   //$("#StepInto").html("<p>"+"</p>");
+     $("#part2").append("<div>"+"hello"+"</div>");
 }
 
 
-
-/*var countdownTime = 124;
-
-setInterval(function() {
-    countdownTime--
-    $("#").html(countdownTime);
-    
-  }, 1000);
-
-
-
-
-
-
-//pro-strat
-  //$().html("")
-  //$().html(<p>gbksbgk</p>)
