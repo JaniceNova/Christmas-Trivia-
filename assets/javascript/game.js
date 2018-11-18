19e75decdbd34f6ca6ecdb911e52aec7fa4d7f23
Bryan Liu <bryanliu@macbook-pro.attlocal.net>
2018-11-17 17:05:02 -0800

    done button done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,7 @@
-var countdownTime = 5;
-var score = 0;
+var countdownTime = 124;
+var correctScore = 0;
+var incorrectScore = 0;
+var unansweredScore = 5;
 
 document.getElementById("StepInto").onclick = function() {myFunction()};
 
@@ -12,21 +14,24 @@ $( "#triviaQuiz" ).empty();
 setInterval(function() {
     countdownTime--
     $("#part2").html(countdownTime);
+    $("#part2").append("<br>" + "<button>" + "Done" + "</button>");
+
+
+
+
+
+
+
      if (countdownTime === 0) {
-
     $( "#triviaQuiz" ).empty();
-    $("#part2").html("Score:");
-    $("#part2").append("<br>" + score);
-
+    $("#part2").html("<br>" + "Correct Answers: " + correctScore + "<br>");
+    $("#part2").append("<br>" + "Incorrect Answers: " + incorrectScore + "<br>");
+    $("#part2").append("<br>" + "Unanswered Questions: " + unansweredScore + "<br>");
     countdownTime++
-    
-
-
   }
   }, 1000);
 
  
-
 
 }
 
